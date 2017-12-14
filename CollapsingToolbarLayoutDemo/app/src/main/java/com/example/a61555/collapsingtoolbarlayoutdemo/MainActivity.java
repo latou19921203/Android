ec90a734f15e342aeb1ba67633b885d9b40491e8
latou19921203 <latou.main@foxmail.com>
2017-12-14 17:22:51 +0800

    2017年12月14日17点21分
    使用CollapsingToolbarLayout实现带背景图的可滚动标题栏效果

--- CollapsingToolbarLayoutDemo/app/src/main/java/com/example/a61555/collapsingtoolbarlayoutdemo/MainActivity.java
+++ CollapsingToolbarLayoutDemo/app/src/main/java/com/example/a61555/collapsingtoolbarlayoutdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.a61555.collapsingtoolbarlayoutdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_left2);//设置导航栏图标
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Back", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
